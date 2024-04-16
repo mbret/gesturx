0dba39792dcb7d05fd0e58de7d018144e40adfbe
maxime <bret.maxime@gmail.com>
2024-04-16 21:51:18 +0200

    feat: added test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from "vitest/config"
+import { defineConfig } from "vitest/config";
 
 export default defineConfig(() => ({
   test: {
     environment: "happy-dom",
-    coverage: {
-      reportsDirectory: `./.test/coverage`,
-    },
+    coverage: {},
   },
-}))
+}));
