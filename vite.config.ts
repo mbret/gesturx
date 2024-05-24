db751d99eaaea16b85ccb5fda1e90a841ee69e20
maxime <bret.maxime@gmail.com>
2024-05-24 16:00:22 +0200

    feat: added lib mode build

--- vite.config.ts
+++ vite.config.ts
@@ -4,29 +4,35 @@ import { resolve } from "path"
 import externals from "rollup-plugin-node-externals"
 import react from "@vitejs/plugin-react"
 
-export default defineConfig(({ mode }) => ({
-  build: {
-    minify: false,
-    // lib: {
-    //   entry: resolve(__dirname, `src/core/index.ts`),
-    //   name: "gesturx",
-    //   fileName: "index",
-    // },
-    emptyOutDir: mode !== `development`,
-    sourcemap: true,
-  },
-  plugins: [
-    react(),
-    {
-      enforce: `pre`,
-      ...externals({
-        peerDeps: true,
-        deps: true,
-        devDeps: true,
+export default defineConfig(({ mode }) => {
+  const libMode = process.env.LIB_MODE === "true"
+
+  return {
+    build: {
+      minify: false,
+      ...(libMode && {
+        lib: {
+          entry: resolve(__dirname, `src/core/index.ts`),
+          name: "gesturx",
+          fileName: "index",
+        },
       }),
+      emptyOutDir: mode !== `development`,
+      sourcemap: true,
     },
-    dts({
-      entryRoot: "src",
-    }),
-  ],
-}))
+    plugins: [
+      react(),
+      {
+        enforce: `pre`,
+        ...externals({
+          peerDeps: true,
+          deps: true,
+          devDeps: true,
+        }),
+      },
+      dts({
+        entryRoot: "src",
+      }),
+    ],
+  }
+})
