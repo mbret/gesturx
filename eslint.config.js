65203da75e11ff5386b5153d5c467664ab424779
Maxime Bret <bret.maxime@gmail.com>
2024-06-17 16:52:19 +0200

    fix: lint

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,11 @@ import globals from "globals"
 import pluginJs from "@eslint/js"
 import tseslint from "typescript-eslint"
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js"
-import { fixupConfigRules, includeIgnoreFile } from "@eslint/compat"
-import path from "node:path"
+
 import { fileURLToPath } from "node:url"
+
+import { includeIgnoreFile } from "@eslint/compat"
+import path from "node:path"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -13,12 +15,14 @@ const gitignorePath = path.resolve(__dirname, ".gitignore")
 export default [
   includeIgnoreFile(gitignorePath),
   {
-    languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } },
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
   },
-  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
+  pluginReactConfig,
   {
     rules: {
       "react/react-in-jsx-scope": "off",
