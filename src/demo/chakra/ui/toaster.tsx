9267b5e71bd7bcaef452162174bd6413842898fb
mbret <bret.maxime@gmail.com>
2025-06-15 14:42:07 +0200

    fix: fix toast weirdbess

--- src/demo/chakra/ui/toaster.tsx
+++ src/demo/chakra/ui/toaster.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import {
 	Toaster as ChakraToaster,
 	Portal,
@@ -8,36 +6,40 @@ import {
 	Toast,
 	createToaster,
 } from "@chakra-ui/react";
+import { memo } from "react";
 
 export const toaster = createToaster({
-	placement: "bottom-end",
+	placement: "bottom",
 	pauseOnPageIdle: true,
+	overlap: true,
 });
 
-export const Toaster = () => {
+export const Toaster = memo(() => {
 	return (
 		<Portal>
 			<ChakraToaster toaster={toaster} insetInline={{ mdDown: "4" }}>
-				{(toast) => (
-					<Toast.Root width={{ md: "sm" }}>
-						{toast.type === "loading" ? (
-							<Spinner size="sm" color="blue.solid" />
-						) : (
-							<Toast.Indicator />
-						)}
-						<Stack gap="1" flex="1" maxWidth="100%">
-							{toast.title && <Toast.Title>{toast.title}</Toast.Title>}
-							{toast.description && (
-								<Toast.Description>{toast.description}</Toast.Description>
+				{(toast) => {
+					return (
+						<Toast.Root width={{ md: "sm" }}>
+							{toast.type === "loading" ? (
+								<Spinner size="sm" color="blue.solid" />
+							) : (
+								<Toast.Indicator />
 							)}
-						</Stack>
-						{toast.action && (
-							<Toast.ActionTrigger>{toast.action.label}</Toast.ActionTrigger>
-						)}
-						{toast.meta?.closable && <Toast.CloseTrigger />}
-					</Toast.Root>
-				)}
+							<Stack gap="1" flex="1" maxWidth="100%">
+								{toast.title && <Toast.Title>{toast.title}</Toast.Title>}
+								{toast.description && (
+									<Toast.Description>{toast.description}</Toast.Description>
+								)}
+							</Stack>
+							{toast.action && (
+								<Toast.ActionTrigger>{toast.action.label}</Toast.ActionTrigger>
+							)}
+							{toast.closable && <Toast.CloseTrigger />}
+						</Toast.Root>
+					);
+				}}
 			</ChakraToaster>
 		</Portal>
 	);
-};
+});
--- src/demo/main.tsx
+++ src/demo/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Provider } from "./chakra/ui/provider.tsx";
+import { Provider } from "./chakra/ui/provider";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById("root")!).render(
